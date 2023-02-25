d35ab0bf37e14fc3122e8415586b92ae6eab587e
ggmxmx <80310226+ggmxmx@users.noreply.github.com>
2023-02-25 16:57:37 +0200

    ...

--- src/components/Amount.js
+++ src/components/Amount.js
@@ -1,58 +1,82 @@
-import React, { useContext, useState  } from 'react';
+import React, { useContext, useState } from 'react';
 import { ImageBackground, Pressable, StyleSheet, Text, View, Image } from 'react-native';
 import ResturantContext from '../../store/resturantContext';
 
 const Card = props => {
   const amountSaver = amount
-  const { image, price, name } = props;
- 
- const {cart , setCart} =  useContext(ResturantContext)
+  const { image, price, name , hideAddRemove } = props;
 
- const [amount, setamount] = useState(0);
+  const { cart, setCart } = useContext(ResturantContext)
+  const orderIndex = cart?.findIndex(order => order.name === name);
 
- const orderIndex = cart?.findIndex(order => order.name === name );  
+  const amount = orderIndex == -1 ? 0 : cart[orderIndex]?.amount
+  // const [amount, setamount] = useState(initAmount || 0 );
 
- const removeOrderFromCart = () => {
-      
- }
 
-  amountHandler = () => {
-    if (amount < 1) {
-      setamount(0)
-      removeOrderFromCart();
-      return ;
-    } else { 
-      setamount(amount - 1)
-    }
 
-   
-    var cartCopy = cart ; 
+  const removeOrderFromCart = () => {
+    const filterdCart = cart.filter((order) => {
+      return order.name !== name;
+    })
 
+    console.log('cart: ', filterdCart);
+    setCart([...filterdCart]);
+  }
+
+  amountHandler = (newAmont) => {
+    console.log('dddd', newAmont);
+  }
+
+  console.log("amount: ", amount);
+
+
+  const updateCart = (newAmont) => {
+    var cartCopy = cart;
     const order = {
-      name , 
-      price , 
-      image , 
-      finallPrice:price * amount , 
+      name,
+      price,
+      image,
+      amount: newAmont,
+      finallPrice: price * newAmont,
     }
 
-    if(orderIndex == -1 ) {
+    if (orderIndex == -1) {
       cartCopy.push(order);
-    }else {
-      cartCopy[orderIndex] = order ; 
+    } else {
+      cartCopy[orderIndex] = order;
     }
+
+    console.log("cartCopy: " , cartCopy);
 
     setCart([...cartCopy])
 
-  } 
-
-  amountHandler2 = () => {
-    setamount(amount + 1)
   }
-  const finallPrice = () =>{
+
+  const onPlusPress = () => {
+    const newAmont = amount + 1;
+    // setamount(newAmont);
+    updateCart(newAmont);
+  }
+
+  const onMinusPress = () => {
+    const newAmont = amount - 1
+
+    if (newAmont < 1) {
+      // setamount(0)
+      removeOrderFromCart();
+      return;
+    } else {
+      setamount(newAmont)
+      updateCart(newAmont);
+    }
+  }
+
+
+  const finallPrice = () => {
     return (
-      <Text style = {styles.Text}>{price * amount}</Text>
+      <Text style={styles.Text}>{price * amount}</Text>
     )
-}
+  }
 
 
 
@@ -66,16 +90,22 @@ const Card = props => {
           <Text style={styles.amountStyle}>{"price: " + price}</Text>
         </View>
       </View>
-      <Pressable onPress={amountHandler2}>
-        <Text style={styles.amountStyle}>+</Text>
-      </Pressable>
+
+      {!hideAddRemove &&
+        <Pressable onPress={onPlusPress}>
+          <Text style={styles.amountStyle}>+</Text>
+        </Pressable>
+      }
 
       <Text style={styles.amountStyle}>{'amount: ' + amount}</Text>
 
-      <Pressable onPress={amountHandler}>
-        <Text style={styles.amountStyle}>-</Text>
-        <Text style = {styles.amountStyle}>{"Finall price: " + price*amount}</Text>
-      </Pressable>
+      {!hideAddRemove &&
+        <Pressable onPress={onMinusPress}>
+          <Text style={styles.amountStyle}>-</Text>
+        </Pressable>
+      }
+
+      <Text style={styles.amountStyle}>{"Finall price: " + price * amount}</Text>
     </View>
 
   );
@@ -83,15 +113,15 @@ const Card = props => {
 
 const styles = StyleSheet.create({
   cardContainer: {
-    width: "80%", 
+    width: "80%",
     // height: 140,
     borderWidth: 5,
     borderColor: 'rgb(159,0,205)',
     alignSelf: 'center',
     marginBottom: 10,
 
-    borderRadius:15,
-        marginTop: 10
+    borderRadius: 15,
+    marginTop: 10
 
   },
   Text: {
@@ -112,19 +142,19 @@ const styles = StyleSheet.create({
     color: 'rgba(0,0,0,0.8)'
   },
   imageStyle: {
-    width:90 , 
-    height:90 ,
-    marginTop:15 , 
-    marginLeft:15 ,
-    borderRadius:15 
+    width: 90,
+    height: 90,
+    marginTop: 15,
+    marginLeft: 15,
+    borderRadius: 15
   },
   productContainer: {
     flexDirection: 'row',
     width: '100%'
   },
-  textContainer:{
-    paddingTop:30 , 
-    paddingLeft:30 ,
+  textContainer: {
+    paddingTop: 30,
+    paddingLeft: 30,
   }
 
 });
--- src/screens/Buy.js
+++ src/screens/Buy.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import {View, ImageBackground, StyleSheet , ScrollView} from "react-native";
 import Images from "../assets/images";
 import finallPrice from "../components/Amount"
 import { useState } from "react";
+import ResturantContext from "../../store/resturantContext";
+import Card from "../components/Amount";
 
 const Buy = () =>{
-    const [background, setbackground] = useState(Images.homeBackground)
+
+    const {cart} = useContext(ResturantContext) ;
+
+    console.log("cart: " , cart);
+
+    const renderCart = () => {
+        return cart.map(order => <Card {...order} hideAddRemove={true} /> )
+    }
+
     return(
         <View style = {{flex:1}}>
-            <ImageBackground source={background} style = {{flex:1}}>
+            <ImageBackground source={Images.homeBackground()} style = {{flex:1}}>
                 <ScrollView>
-                    {finallPrice}
+                    {renderCart()}
                 </ScrollView>
             </ImageBackground>
         </View>
